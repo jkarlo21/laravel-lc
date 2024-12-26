002e39d3d18e9ccfccc1c08e07417b6e782ad31a
jkarlo21 <johnk.achico@gmail.com>
2024-12-26 19:02:40 +0800

    Episode 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,16 @@
     <x-slot:heading>
         Job Listing
     </x-slot:heading>
-    @foreach ($jobs as $job)
-        <ul>
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year
-                </a>
-            </li>
-        </ul>
-    @endforeach
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year
+                </div>
+               
+            </a>
+        @endforeach
+    </div>
+
 </x-layout>
